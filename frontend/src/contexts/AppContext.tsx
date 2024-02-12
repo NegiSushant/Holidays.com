d32ea42aca6b114c5791710ab08d71db82fe9873
Sushant Singh Negi <sushantsinghnegi1998@gmail.com>
2024-02-12 19:26:11 +0530

    updations

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -1,4 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import Toast from "../components/Toast";
+import { useQuery } from "react-query";
+import * as apiClient from "../api-client";
+
 
 type ToastMessage = {
   message: string;
@@ -7,6 +11,7 @@ type ToastMessage = {
 
 type AppContext = {
   showToast: (toastMessage: ToastMessage) => void;
+  isLoggedIn: boolean;
 };
 
 const AppContext = React.createContext<AppContext | undefined>(undefined);
@@ -16,10 +21,27 @@ export const AppContextProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
+  const [toast, setToast] = useState<ToastMessage | undefined>(undefined);
+
+  const { isError } = useQuery("validateToken", apiClient.validateToken, {
+    retry: false,
+  });
   return (
     <AppContext.Provider
-      value={{ showToast: (toastMessage) => console.log(toastMessage) }}
+      value={{
+        showToast: (toastMessage) => {
+          setToast(toastMessage);
+        },
+        isLoggedIn: !isError,
+      }}
     >
+      {toast && (
+        <Toast
+          message={toast.message}
+          type={toast.type}
+          onClose={() => setToast(undefined)}
+        />
+      )}
       {children}
     </AppContext.Provider>
   );
