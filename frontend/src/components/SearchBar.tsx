2b8d09f6e8634e12d85d1e354d6cdf8981b6f4fb
Sushant Singh Negi <sushantsinghnegi1998@gmail.com>
2024-04-23 19:51:00 +0530

    updation

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -99,7 +99,7 @@ const SearchBar = () => {
         />
       </div>
       <div className="flex gap-1">
-        <button className="w-2/3 bg-green-500 text-white h-full p-2 font-bold text-xl hover:bg-blue-500">
+        <button className="w-2/3 bg-green-500 text-white h-full p-2 font-bold text-xl hover:bg-orange-500">
           Search
         </button>
         <button className="w-1/3 bg-red-600 text-white h-full p-2 font-bold text-xl hover:bg-red-500">
